8f065c62c420f480fcf1a29e46cb4974c0d9789f
Gustavo Silva <silva95gustavo@gmail.com>
2016-06-11 22:48:20 +0100

    Added missing file js/main.js.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.modal').on('hidden.bs.modal', function () {
+	$(this).find('.modal-body iframe').each(function(){
+	  var video = $(this).attr("src");
+	  $(this).attr("src", "");
+	  $(this).attr("src", video);
+	});
+});
+console.log("c");
